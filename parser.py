12dcfea8d1bd0449646da9dceafb02d30dd48282
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-02-10 09:33:11 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -12,6 +12,7 @@ def load_Exac_constraints(data_folder):
         _id = rec["symbol"]
         process_key = lambda k: k.replace(" ","_").lower()
         rec = dict_convert(rec,keyfn=process_key)
+        rec = dict_sweep(rec,vals=[np.nan])
         results.setdefault(_id,[]).append(rec)
     for _id,docs in results.items():
         doc = {"_id": _id, "Exac_constraints" : docs}
